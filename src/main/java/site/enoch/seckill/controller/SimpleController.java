4310d3096000797863d4c0806a2891070a2d1160
Ethanim <1045150582@qq.com>
2018-05-08 23:27:47 +0800

    注释多余内容
    注释多余内容

--- src/main/java/site/enoch/seckill/controller/SimpleController.java
+++ src/main/java/site/enoch/seckill/controller/SimpleController.java
@@ -1,19 +1,19 @@
 package site.enoch.seckill.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import site.enoch.seckill.rabbitmq.MQSender;
+//import site.enoch.seckill.rabbitmq.MQSender;
 import site.enoch.seckill.result.Result;
 
 @Controller
 @RequestMapping("/demo")
 public class SimpleController {
 
-	@Autowired
-	private MQSender sender;
+	//@Autowired
+	//private MQSender sender;
 
 	@RequestMapping("/hello")
 	@ResponseBody
--- src/main/java/site/enoch/seckill/rabbitmq/MQConfig.java
+++ src/main/java/site/enoch/seckill/rabbitmq/MQConfig.java
@@ -1,13 +1,13 @@
 package site.enoch.seckill.rabbitmq;
 
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.core.HeadersExchange;
+//import java.util.HashMap;
+//import java.util.Map;
+//import org.springframework.amqp.core.Binding;
+//import org.springframework.amqp.core.BindingBuilder;
+//import org.springframework.amqp.core.FanoutExchange;
+//import org.springframework.amqp.core.HeadersExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+//import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
